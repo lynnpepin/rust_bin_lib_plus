222861f9e9eb7a0335de63b064c12e965110003e
lynn <lynn@lynndotpy.xyz>
2023-02-22 20:31:28 -0500

    Add CLI parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,42 @@
 use lib::test;
+use clap::{Parser, Subcommand};
+
+// Parse commands and arguments from the CLI
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// If verbose is set, print extra messages while running.
+    #[arg(short, long, default_value_t = false)]
+    verbose: bool,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Create a structure (example command)
+    Create {
+        #[arg(short, long)]
+        key: Option<String>,
+
+        #[arg(short, long="val")]
+        value: Option<i64>,
+
+        #[arg(short='X', long)]
+        password: Option<String>,
+    },
+    /// Delete the structure (example command)
+    Delete {
+        #[arg(short, long)]
+        force: Option<String>,
+    },
+}
+
 
 pub fn main() {
-    test();
+    //test();
+    let cli = Cli::parse();
+
+    println!("{cli:?}");
 }
